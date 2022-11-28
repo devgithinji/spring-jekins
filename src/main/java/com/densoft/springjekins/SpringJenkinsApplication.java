9d0d0f90b2e8e065fcb50b9dbd5b58db4968eed3
dennis githinji <wakahiad@gmail.com>
2022-11-28 12:32:03 +0300

    first commit

---
+++ src/main/java/com/densoft/springjekins/SpringJenkinsApplication.java
@@ -0,0 +1,25 @@
+package com.densoft.springjekins;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+
+    static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+
+    @PostConstruct
+    public void intt() {
+        logger.info("Application started...");
+    }
+
+    public static void main(String[] args) {
+        logger.info("Application executed ...");
+        SpringApplication.run(SpringJenkinsApplication.class, args);
+    }
+
+}
