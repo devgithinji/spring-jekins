fe491ca37e4852d2842dd105a97d6c19cb3d858e
dennis githinji <wakahiad@gmail.com>
2022-11-28 13:34:11 +0300

    controller add

---
+++ src/main/java/com/densoft/springjekins/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.densoft.springjekins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping
+    public String sayHello() {
+        return "hello jenkins";
+    }
+}
