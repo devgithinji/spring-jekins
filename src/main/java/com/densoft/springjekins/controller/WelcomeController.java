e73d892cff969349e55f9f1d4657c430b5ea1b82
dennis githinji <wakahiad@gmail.com>
2022-11-28 15:54:18 +0300

    fix

--- src/main/java/com/densoft/springjekins/controller/WelcomeController.java
+++ src/main/java/com/densoft/springjekins/controller/WelcomeController.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
-    @GetMapping
-    public String sayHello() {
-        return "hello jenkins";
-    }
-
-
-    @GetMapping
-    public String sayHelloPeople() {
-        return "hello devs";
-    }
+//    @GetMapping
+//    public String sayHello() {
+//        return "hello jenkins";
+//    }
+//
+//
+//    @GetMapping
+//    public String sayHelloPeople() {
+//        return "hello devs";
+//    }
 }
